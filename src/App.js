ac2e01c163ac66f498c52edc9632d3c787e9dce3
David Marino <david.marino@bayer.com>
2023-02-17 16:12:52 -0600

    added cookies to save username and password

--- src/App.js
+++ src/App.js
@@ -3,27 +3,30 @@ import Clock from './component/Clock'
 import StartStop from './component/StartStop'
 import Submit from './component/Submit'
 import './style/App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './component/Login'
+import { useCookies } from 'react-cookie'
 
 const App = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route
-                    path="/"
-                    element={
-                        <div>
-                            <Clock />
-                            <StartStop />
-                            <Submit />
-                        </div>
-                    }
-                />
-                <Route path="/login" element={<Login />} />
-            </Routes>
-        </BrowserRouter>
-    )
+	const [cookies, setCookie] = useCookies()
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route exact path="/" element={cookies.username === undefined ? <Navigate to="/login" /> : <Navigate to="/clock" />} />
+				<Route
+					path="/clock"
+					element={
+						<div>
+							<Clock />
+							<StartStop />
+							<Submit />
+						</div>
+					}
+				/>
+				<Route path="/login" element={<Login />} />
+			</Routes>
+		</BrowserRouter>
+	)
 }
 
 export default App
--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,12 +1,47 @@
-
-
+import "../style/Login.css"
+import React, { useState } from "react"
+import { useCookies } from 'react-cookie'
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+    const [cookies, setCookie] = useCookies()
 
+    const [remember, setRemember] = useState(false)
+    const [username, setUsername] = useState()
+    const [password, setPassword] = useState()
+
+    const navigate = useNavigate()
+
+    const saveToCookies = () => {
+        if (remember) {
+            setCookie('username', username, { path: '/' });
+            setCookie('password', password, { path: '/' });
+        }
+        console.log(cookies)
+    }
 
     return (
         <>
-            <h1>Hello</h1>
+            <div className="center">
+                <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+                <div class="form-floating">
+                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" onChange={(e) => setUsername(e.target.value)} />
+                    <label for="floatingInput">Email address</label>
+                </div>
+                <div class="form-floating">
+                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+                    <label for="floatingPassword">Password</label>
+                </div>
+
+                <div class="checkbox mb-3">
+                    <label>
+                        <input type="checkbox" value="remember-me" onClick={() => setRemember(!remember)} /> Remember me
+                    </label>
+                </div>
+                <button class="w-100 btn btn-lg btn-primary" type="submit" onClick={saveToCookies}>Sign in</button>
+                <a href={""} onClick={() => navigate('/clock')}>register</a>
+            </div>
         </>
     )
 }
